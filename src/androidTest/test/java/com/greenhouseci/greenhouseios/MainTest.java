e60b62b76729416e6741554d09f5784ba40e5f1a
thirumalaivigneshmohan <thirumalaivignesh.m@cognizant.com>
2016-02-25 17:10:31 +0530

    Update MainTest.java

--- src/androidTest/test/java/com/greenhouseci/greenhouseios/MainTest.java
+++ src/androidTest/test/java/com/greenhouseci/greenhouseios/MainTest.java
@@ -8,27 +8,8 @@ public class MainTest extends BaseTest {
 
 	@Test
 	public void testFindViewSuccess() {
-		WebElement textView = this.driver.findElementByName("text_hello_world");
-		Assert.assertNotNull(textView);
-		Assert.assertEquals(textView.getText(), "Hello world!");
+	
 	}
 
-	@Test
-	public void testFindViewFail() {
-		WebElement textView = this.driver.findElementByName("text_hello_world");
-		Assert.assertNotNull(textView);
-		Assert.assertEquals(textView.getText(), "Hello world");
-	}
 
-	@Test
-	public void testButtonClickSuccess() {
-		WebElement textView = this.driver.findElementByName("text_hello_world");
-		Assert.assertNotNull(textView);
-
-		WebElement button = this.driver.findElementByName("button_change_text");
-		Assert.assertNotNull(button);
-		button.click();
-
-		Assert.assertEquals(textView.getText(), "Changed hello world");
-	}
 }
